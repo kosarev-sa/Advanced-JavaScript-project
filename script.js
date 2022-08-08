f4b010b3ca594e32db01bedbe75be95e56924853
kosarev-sa <kosarev-sa@mail.ru>
2022-08-08 19:00:20 +0300

    + tasks_2 (added calculateTotalPrice)

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
 const goods = [
-    {title: 'Basket', price: 1500},
-    {title: 'Vase', price: 500},
-    {title: 'Pots', price: 350},
-    {title: 'Rocking chair', price: 10000},
+    {
+        title: 'Basket',
+        price: 1500,
+        img_url: 'https://static.insales-cdn.com/images/products/1/7243/525401163/_11_.jpg'
+    },
+    {
+        title: 'Vase',
+        price: 500,
+        matter: 'bast',
+        img_url: 'https://cdn1.ozone.ru/s3/multimedia-p/wc1200/6007760221.jpg'
+    },
+    {
+        title: 'Pots',
+        price: 350,
+        img_url: 'https://cs1.livemaster.ru/storage/13/a0/d6a03049b3af96d7c6178894f9rc.jpg'
+    },
+    {
+        title: 'Rocking chair',
+        price: 10000,
+        img_url: 'https://l-dacha.ru/upload/iblock/c37/c378b597409b96786afdf5b169dd50ae.png'
+    },
 ];
 
-const renderGoodsItem = (title, price, matter = 'withe') => {
-    return `
-    <div class="goods-item">
-      <h3>${title}</h3>
-      <h4>${matter}</h4>
-      <p>${price}</p>
+class GoodsItem {
+    constructor({title = 'wickerwork', price = 0, matter = 'withe', img_url}) {
+        this.title = title;
+        this.price = price;
+        this.matter = matter;
+        this.image_style = "background-image: url(\'" + img_url + "\'); background-size: 100%"
+    }
+
+    render() {
+        return `
+    <div class="goods-item" style="${this.image_style}">
+      <h3>${this.title}</h3>
+      <h4>${this.matter}</h4>
+      <p>${this.price}</p>
     </div>
   `;
-};
-
-const renderGoodsList = (list) => {
-
-    let goodsList = list.map(item => {
-        let {title, price} = item;
-        return renderGoodsItem(title, price)
-    }).join('');
-
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+    items = [];
+
+    fetchGoods() {
+        this.items = goods;
+    }
+
+    render() {
+        const goods = this.items.map(item => {
+            const goodItem = new GoodsItem(item);
+            return goodItem.render()
+        }).join('');
+        debugger
+        document.querySelector('.goods-list').innerHTML = goods;
+    }
+
+    calculateTotalPrice() {
+        return this.items.reduce((total, amount) => {return total + amount.price;}, 0)
+    }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
