08e474274dd67c3a99a80b71e266c9668fd937a8
kosarev-sa <kosarev-sa@mail.ru>
2022-08-12 15:19:42 +0300

    + tasks_3 (added Pomises)

--- script.js
+++ script.js
@@ -1,30 +1,75 @@
-const goods = [
-    {
-        title: 'Basket',
-        price: 1500,
-        img_url: 'https://static.insales-cdn.com/images/products/1/7243/525401163/_11_.jpg'
-    },
-    {
-        title: 'Vase',
-        price: 500,
-        matter: 'bast',
-        img_url: 'https://cdn1.ozone.ru/s3/multimedia-p/wc1200/6007760221.jpg'
-    },
-    {
-        title: 'Pots',
-        price: 350,
-        img_url: 'https://cs1.livemaster.ru/storage/13/a0/d6a03049b3af96d7c6178894f9rc.jpg'
-    },
-    {
-        title: 'Rocking chair',
-        price: 10000,
-        img_url: 'https://l-dacha.ru/upload/iblock/c37/c378b597409b96786afdf5b169dd50ae.png'
-    },
-];
+//const goods = [
+//    {
+//        title: 'Basket',
+//        price: 1500,
+//        img_url: 'https://static.insales-cdn.com/images/products/1/7243/525401163/_11_.jpg'
+//    },
+//    {
+//        title: 'Vase',
+//        price: 500,
+//        matter: 'bast',
+//        img_url: 'https://cdn1.ozone.ru/s3/multimedia-p/wc1200/6007760221.jpg'
+//    },
+//    {
+//        title: 'Pots',
+//        price: 350,
+//        img_url: 'https://cs1.livemaster.ru/storage/13/a0/d6a03049b3af96d7c6178894f9rc.jpg'
+//    },
+//    {
+//        title: 'Rocking chair',
+//        price: 10000,
+//        img_url: 'https://l-dacha.ru/upload/iblock/c37/c378b597409b96786afdf5b169dd50ae.png'
+//    },
+//];
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+//function makeGETRequest(url, callback) {
+//    var xhr;
+//    if(window.XMLHttpRequest) {
+//        xhr =new XMLHttpRequest();
+//        }
+//    else if(window.ActiveXObject) {
+//        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//        }
+//    xhr.onreadystatechange = function() {
+//    if(xhr.readyState === 4) {
+//        callback(xhr.responseText);
+//        }
+//    }
+//    xhr.open('GET', url, true);
+//    xhr.send();
+//}
+
+function makeGETRequest(url) {
+    return new Promise((resolve, reject) => {
+        var xhr;
+        if (window.XMLHttpRequest) {
+            xhr =new XMLHttpRequest();
+            }
+        else if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(xhr.responseText);
+                } else {
+                    reject(`'Error, xhr.status = ${xhr.status}'`);
+                }
+            }
+        };
+        xhr.onerror = function (error) {
+            reject(err);
+        };
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
+    }
 
 class GoodsItem {
-    constructor({title = 'wickerwork', price = 0, matter = 'withe', img_url}) {
-        this.title = title;
+    constructor({product_name = 'wickerwork', price = 0, matter = 'withe', img_url}) {
+        this.title = product_name;
         this.price = price;
         this.matter = matter;
         this.image_style = "background-image: url(\'" + img_url + "\'); background-size: 100%"
@@ -44,8 +89,29 @@ class GoodsItem {
 class GoodsList {
     items = [];
 
-    fetchGoods() {
-        this.items = goods;
+//    fetchGoods() {
+//        this.items = goods;
+//    }
+
+//    fetchGoods(cb) {
+//        makeGETRequest(`${API_URL}/catalogData.json`).then(
+//        (goods) => {
+//            this.items = JSON.parse(goods);
+//            cb()
+//        },
+//        (error) => {
+//            console.log(error)
+//        });
+//    }
+
+        fetchGoods() {
+        return makeGETRequest(`${API_URL}/catalogData.json`).then(
+        (goods) => {
+            this.items = JSON.parse(goods);
+        },
+        (error) => {
+            console.log(error)
+        });
     }
 
     render() {
@@ -53,7 +119,6 @@ class GoodsList {
             const goodItem = new GoodsItem(item);
             return goodItem.render()
         }).join('');
-        debugger
         document.querySelector('.goods-list').innerHTML = goods;
     }
 
@@ -63,5 +128,9 @@ class GoodsList {
 }
 
 const goodsList = new GoodsList();
-goodsList.fetchGoods();
-goodsList.render();
+//goodsList.fetchGoods(() => {
+//    goodsList.render();
+//});
+goodsList.fetchGoods().then(() => {
+    goodsList.render();
+});
