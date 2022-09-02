18a10ca95bbee2730c8bf9967df56ef7b0b44d88
kosarev-sa <kosarev-sa@mail.ru>
2022-09-02 14:01:31 +0300

    + tasks_7 (added node.js server, online put, delete)

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
-const GET_GOODS_ITEMS = `${API_URL}catalogData.json`
+// const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 
-function service(url) {
-    return fetch(url)
+const API_URL = 'http://localhost:8000';
+const GET_GOODS_ITEMS = `${API_URL}/goods.json`;
+const GET_BASKET_GOODS = `${API_URL}/basket_goods`;
+
+function service(url, method = "GET", body) {
+    return fetch(url, {
+        headers: Object.assign({}, body ? {
+            'Content-Type': 'application/json; charset=utf-8'
+        } : {}),
+        method,
+        body: JSON.stringify(body)
+    })
         .then((res) => res.json())
 }
 
@@ -37,8 +46,16 @@ function init() {
           <div class="goods-item">
           <h3>{{ item.product_name }}</h3>
           <p>{{ item.price }}</p>
+          <custom-button @click="addGood">добавить</custom-button>
           </div>
-        `
+        `,
+        methods: {
+            addGood() {
+                service(GET_BASKET_GOODS, 'PUT', {
+                    id: this.item.id
+                })
+            }
+        }
     })
 
     const basketGoods = Vue.component('basket-goods', {
@@ -56,14 +73,49 @@ function init() {
               <div class="basket-card__header__delete-icon" v-on:click="$emit('closeclick')"></div>
             </div>
             <div class="basket-card__content">
-              basket info
+              <basket-goods-item
+                  v-for="item in basketGoodsItems" :item="item" @add="addGood" @delete="deleteBasketGood" >
+              </basket-goods-item>
             </div>
           </div>
           </div>
         `,
         mounted() {
-
+            service(GET_BASKET_GOODS).then((data) => {
+                this.basketGoodsItems = data
+            })
+        },
+        methods: {
+            addGood(id) {
+                service(GET_BASKET_GOODS, 'PUT', {
+                    id
+                }).then((data) => {
+                    this.basketGoodsItems = data
+                })
+            },
+            deleteBasketGood(id) {
+                service(GET_BASKET_GOODS, 'DELETE', {
+                    id
+                }).then((data) => {
+                    this.basketGoodsItems = data
+                })
+            }
         }
+    })
+
+    Vue.component('basket-goods-item', {
+        props: [
+            'item'
+        ],
+        template: `
+          <div class="basket-card__content___item">
+          <h3>{{ item?.data?.product_name }}</h3>
+          <div>count: {{ item?.count }}</div>
+          <div>total: {{ item?.total }}</div>
+          <custom-button @click="$emit('add', item.data.id)">добавить | +1 |</custom-button>
+          <custom-button @click="$emit('delete', item.data.id)">удалить | -1 |</custom-button>
+          </div>
+        `
     })
 
     const app = new Vue({
